636c8a4c33ce6a701b464774a934557370318636
Arhises <roma1039@gmail.com>
2024-05-13 16:45:41 +0300

    hw07 added

--- HW06/data.js
+++ HW06/data.js
@@ -9,36 +9,36 @@ const DataJS = `[
     {
         "class": "item",
         "imgUrl": "/HW06/imgs/beard dude.svg",
-        "name": "ELLERY X M'O CAPSULE",
-        "price": "$52.00",
+        "name": "item - 01",
+        "price": "$1.00",
         "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
     },
     {
         "class": "item",
         "imgUrl": "/HW06/imgs/beard dude.svg",
-        "name": "ELLERY X M'O CAPSULE",
-        "price": "$52.00",
+        "name": "item - 02",
+        "price": "$5.00",
         "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
     },
     {
         "class": "item",
         "imgUrl": "/HW06/imgs/beard dude.svg",
-        "name": "ELLERY X M'O CAPSULE",
-        "price": "$52.00",
+        "name": "item - 03",
+        "price": "$532.00",
         "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
     },
     {
         "class": "item",
         "imgUrl": "/HW06/imgs/beard dude.svg",
-        "name": "ELLERY X M'O CAPSULE",
-        "price": "$52.00",
+        "name": "item - 04",
+        "price": "$524.00",
         "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
     },
     {
         "class": "item",
         "imgUrl": "/HW06/imgs/beard dude.svg",
-        "name": "ELLERY X M'O CAPSULE",
-        "price": "$52.00",
+        "name": "item - 05",
+        "price": "$2.00",
         "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
     }
 ]`;
--- HW06/script.js
+++ HW06/script.js
@@ -2,7 +2,8 @@ const data = JSON.parse(DataJS);
 const template = document.querySelector("#productTemplate");
 const container = document.createElement("div");
 container.classList.add("items");
-document.body.appendChild(container);
+container.classList.add("center");
+document.body.insertBefore(container, document.body.firstChild);
 
 data.forEach((item) => {
   const clone = document.importNode(template.content, true);
@@ -14,3 +15,30 @@ data.forEach((item) => {
 
   container.appendChild(clone);
 });
+
+const cart = document.querySelector('.product-cart');
+const products = document.querySelectorAll('.items .product');
+let cartList = document.querySelectorAll('.product-cart .product');
+
+console.log(products);
+
+products.forEach(product => {
+  product.addEventListener('click', () => {
+    const productClone = product.cloneNode(true);
+    productClone.addEventListener('click', () => {
+      productClone.remove();
+    });
+    cart.appendChild(productClone);
+    cartList = document.querySelectorAll('.product-cart .product');
+
+    document.querySelectorAll('.product-cart .overlay-text').forEach(item => {
+      item.textContent = 'X - Remove';
+    });
+
+  });
+});
+
+
+
+
+
